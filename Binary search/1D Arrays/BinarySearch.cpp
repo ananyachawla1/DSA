663e31c0352b941d67df4b828390d9ee27cfe1ba
ananyachawla1 <108053283+ananyachawla1@users.noreply.github.com>
2023-07-05 05:17:50 +0530

    Create BinarySearch.cpp

---
+++ Binary search/1D Arrays/BinarySearch.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
+        int low=0;
+        int ans=-1;
+        int high=n-1;
+        while(low<=high)
+        {   
+            int mid=floor(high+low)/2;
+            if(target==nums[mid]){
+                ans=mid;
+            return ans;
+            break;
+            }
+            else if(target>nums[mid])
+            low=mid+1;
+            else if(target<nums[mid])
+            high=mid-1;
+            else
+            return-1;
+            
+        }
+        return ans;
+        
+    }
+};
