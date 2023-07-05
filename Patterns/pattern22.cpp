d3dc104a5a7c517f0eb460abf4784aa80dd9ad4d
ananyachawla1 <108053283+ananyachawla1@users.noreply.github.com>
2023-07-06 05:20:09 +0530

    Pattern 22

---
+++ Patterns/pattern22.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printSquare(int n) {
+        // code here
+        for(int i=0;i<2*n-1;i++)
+        {
+            for(int j=0;j<2*n-1;j++)
+            {
+                cout<<max(abs(n-i-1),abs(n-j-1))+1<<" ";
+            }
+            cout<<endl;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printSquare(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
