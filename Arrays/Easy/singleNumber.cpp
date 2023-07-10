ab1522c5a2c571f556635180fbfacfada8cd5542
ananyachawla1 <108053283+ananyachawla1@users.noreply.github.com>
2023-07-10 23:54:17 +0530

    single number

---
+++ Arrays/Easy/singleNumber.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        for(int i=1;i<n;i+=2)
+        {
+            if(nums[i]!=nums[i-1])
+            {
+                return nums[i-1];
+            }
+        }
+        return nums[n-1];
+        
+    }
+};
