5cb79f49b642e07777fbf06a0518751dd37493c9
ananyachawla1 <108053283+ananyachawla1@users.noreply.github.com>
2023-07-14 06:38:03 +0530

    majority element

---
+++ Arrays/Medium/majorityElement.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        int count=0;
+        int no;
+        
+        for(int i=0;i<n;i++)      
+          {
+              if(count==0)
+              {
+                  count++;
+                  no=nums[i];
+              }
+              else if(nums[i]==no)
+              count++;
+              else
+              count--;
+            }
+            int cnt=0;
+               for(int i=0;i<n;i++)    
+               {
+                   if(no==nums[i])
+                   cnt++;
+               }  
+               if(cnt>n/2)
+               return no;
+                return -1;
+        }
+        
+    
+};
