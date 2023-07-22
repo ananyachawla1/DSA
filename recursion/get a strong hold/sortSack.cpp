475f250ffc7b39af14ad5c1e73b2e15b289839ca
ananyachawla1 <108053283+ananyachawla1@users.noreply.github.com>
2023-07-23 04:52:24 +0530

    sort sack using recursion

---
+++ recursion/get a strong hold/sortSack.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+void reverse(stack<int> &stack,int x)
+{
+    if(stack.size()==0)
+    {
+        stack.push(x);
+    }
+    else
+    {
+        int top=stack.top();
+        stack.pop();
+        reverse(stack,x);
+        stack.push(top);
+    }
+
+}
+void reverseStack(stack<int> &stack) {
+    // Write your code here
+    if(stack.size()>0)
+    {
+        int top=stack.top();
+        stack.pop();
+        reverseStack(stack);
+        reverse(stack,top);
+    }
