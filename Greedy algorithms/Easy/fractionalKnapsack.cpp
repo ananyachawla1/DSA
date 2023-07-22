79c00eb7620653ff9785805556ad20bd76df3f50
ananyachawla1 <108053283+ananyachawla1@users.noreply.github.com>
2023-07-22 05:53:50 +0530

    fractional knapsack new approach

--- Greedy algorithms/Easy/fractionalKnapsack.cpp
+++ Greedy algorithms/Easy/fractionalKnapsack.cpp
@@ -1,34 +1,24 @@
 #include <bits/stdc++.h> 
-
-struct Item {
-   int value;
-   int weight;
-};
- bool static comp(Item a, Item b) {
-         double r1 = (double) a.value / (double) a.weight;
-         double r2 = (double) b.value / (double) b.weight;
-         return r1 > r2;
-      }
+using namespace std;
+bool comp(pair<int,int>&a,pair<int,int>&b)
+{
+     return ((double) a.second / a.first) > ((double) b.second / b.first);
+}
 double maximumValue (vector<pair<int, int>>& items, int n, int w)
 {
-    sort(items, items + n, comp);
-
-      int curWeight = 0;
-      double finalvalue = 0.0;
-
-      for (int i = 0; i < n; i++) {
-
-         if (curWeight + items[i].weight <= w) {
-            curWeight += items[i].weight;
-            finalvalue += items[i].value;
-         } else {
-            int remain = w - curWeight;
-            finalvalue += (items[i].value / (double) items[i].weight) * (double) remain;
+    double ans=0;
+    sort(items.begin(),items.end(),comp);
+    for (int i=0;i<n;i++)
+    {
+        if (items[i].first<=w)
+        {
+            ans+=items[i].second;
+            w-=items[i].first;
+        }
+        else {
+            double temp= (double)(w/(double)items[i].first)*(double)(items[i].second);
+            ans+=temp;
             break;
-         }
-      }
-
-      return finalvalue;
-    // Write your code here.
-    // ITEMS contains {weight, value} pairs.
-}
+        }
+    }
+    return ans;
