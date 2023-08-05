f0f3c70d6f33f9847489f71f1527a18302d09b14
ananyachawla1 <108053283+ananyachawla1@users.noreply.github.com>
2023-08-06 03:01:36 +0530

    reverse stack

---
+++ Binary Trees/Subsequences/reverseStack.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void reverse(stack<int> &stack,int x) {
+    // Write your code here
+    
+    if(stack.size()==0)
+    {
+        stack.push(x);
+    }
+    else
+    {
+        int top=stack.top();
+        stack.pop();
+        reverse(stack,x);
+        stack.push(top);
+}
+}
+void reverseStack(stack<int> &stack) {
+    if(stack.size()>0)
+    {
+        int top=stack.top();
+        stack.pop();
+        reverseStack(stack);
+        reverse(stack,top);
+    }
+}
